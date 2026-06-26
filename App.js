404df199503fd4935b754d86e73b8ee4f2282f7f
Kartheek-22143 <potnurukarthik88@gmail.com>
2026-06-26 15:01:26 +0530

    karthik

---
+++ App.js
@@ -0,0 +1,21 @@
+import StudentForm from "./components/StudentFrom";
+import StudentList from "./components/StudentList";
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Student Management System
+      </h1>
+
+      <StudentForm />
+
+      <StudentList />
+    </div>
+  );
+}
+
+export default App;
+
+
